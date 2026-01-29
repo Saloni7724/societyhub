6916b89c7d9a43a534f708e1e4f92ab2667bc87a
Saloni7724 <sallupatel1007@gmail.com>
2026-01-29 11:04:01 +0530

    Add Admin and User modules

---
+++ src/Admin/Login.jsx
@@ -0,0 +1,16 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+
+const Login = memo(function Login(props) {
+  return (
+    <div>
+      <p> Hello</p>
+    </div>
+  )
+})
+
+Login.propTypes = {
+
+}
+
+export default Login
